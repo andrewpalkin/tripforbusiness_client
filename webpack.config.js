f397cf15edd0fb624d0e7f710c6a3d75bc4a4e45
Andrew Palkin <palkin.andrew@gmail.com>
2018-04-26 12:15:07 +0300

    First upload of project

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const BUILD_DIR = path.join(__dirname, 'dist');
+const APP_DIR = path.join(__dirname, 'src');
+
+const VENDOR_LIBS = [
+    'react', 'react-dom', 'react-router-dom'
+];
+
+let config =  {
+    entry: {
+        bundle: APP_DIR + '/entry.js',
+        vendor: VENDOR_LIBS
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[hash].js',
+        chunkFilename: '[name].[contenthash].js',
+        publicPath: '/'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    babelrc: false,
+                    presets: ["babel-preset-env", "react", "stage-2"],
+                    plugins: ['syntax-dynamic-import']
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: 'file-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        compress: true,
+        port: 9000,
+        host: "andrewp01",
+        disableHostCheck: false,
+        open: true,
+        hot: true
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    chunks: "all",
+                    test: path.resolve(__dirname, "node_modules"),
+                    name: "vendor"
+                }
+            }
+        }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: 'public/index.html'
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ]
+};
+
+module.exports = config;
