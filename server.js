04e307238f608a7a94a6df46f4749d8d8955d4ff
Andrew Palkin <palkin.andrew@gmail.com>
2018-05-06 14:58:47 +0300

    First upload of project

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const path = require('path');
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') {
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack.config.js');
-  app.use(webpackMiddleware(webpack(webpackConfig)));
+    const webpackMiddleware = require('webpack-dev-middleware');
+    const webpack = require('webpack');
+    const webpackConfig = require('./webpack.config.js');
+    app.use(webpackMiddleware(webpack(webpackConfig)));
 } else {
-  app.use(express.static('dist'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-  })
+    app.use(express.static('dist'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public/index.html'));
+    })
 }
 
 app.listen(process.env.PORT || 9000, () => console.log('Listening'));
